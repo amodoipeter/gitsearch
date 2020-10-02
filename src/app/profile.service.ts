ae41b860750f2c97da4c155e12134beb2676b03f
amodoipeter <amodoipeter@gmail.com>
2020-10-02 20:56:50 +0300

    Added a function to fetch profile information to the profile class

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -14,4 +14,9 @@ export class ProfileService {
     console.log("service is now ready");
     this.username = 'amodoipeter';
    }
+
+   getProfileInfo(){
+     return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+     .map(res => res.json());
+   }
 }
